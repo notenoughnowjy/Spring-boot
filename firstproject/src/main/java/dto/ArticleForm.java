f4ac3482f5af2f77e953a0ca6fbe41565960529c
notenoughnowjy <jyp4188@gmail.com>
2023-08-08 17:16:14 +0900

    [2023.08.08] / Spring boot - form format, PostMapping, dto

---
+++ firstproject/src/main/java/dto/ArticleForm.java
@@ -0,0 +1,20 @@
+package dto;
+
+public class ArticleForm {
+
+    private String title;
+    private String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
